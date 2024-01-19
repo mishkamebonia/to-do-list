8e4fb55714113e1e188941582eabe30617ea1844
mishkamebonia <mishkas.mebonia@gmail.com>
2024-01-19 18:45:53 +0400

    added empty tasks display

--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -10,19 +10,25 @@ const Task = ({ todos }: TaskProps) => {
 
   return (
     <div className="tasks">
-      {todos.map((todo) => {
-        return (
-          <div key={todo.id} className="task">
-            <section>
-              <h3>{todo.title}</h3>
-              <p>{todo.about}</p>
-            </section>
-            <button>
-              <i className="fa-solid fa-xmark"></i>
-            </button>
-          </div>
-        );
-      })}
+      {todos.length === 0 ? (
+        <div className="empty-tasks">
+          <p>No tasks</p>
+        </div>
+      ) : (
+        todos.map((todo) => {
+          return (
+            <div key={todo.id} className="task">
+              <section>
+                <h3>{todo.title}</h3>
+                <p>{todo.about}</p>
+              </section>
+              <button>
+                <i className="fa-solid fa-xmark"></i>
+              </button>
+            </div>
+          );
+        })
+      )}
     </div>
   );
 };
