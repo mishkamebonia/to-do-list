3863acdb9c11704c83e083a7bf7cedd4780be3b8
mishkamebonia <mishkas.mebonia@gmail.com>
2024-01-20 21:05:40 +0400

    removed file

--- src/functions/removeTask.tsx
+++
@@ -1,6 +0,0 @@
-import { TodoProps } from "../App";
-
-export const removeTask = (id: string, { todos, setTodos }: TodoProps) => {
-  const findId = todos.filter((todo) => todo.id !== id);
-  setTodos(findId);
-};
