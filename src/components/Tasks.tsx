e9ef3b58596e73d31b273706928313285284b755
mishkamebonia <mishkas.mebonia@gmail.com>
2024-01-20 12:09:25 +0400

    created todo accordion

--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -1,10 +1,22 @@
 import "./Tasks.scss";
 import { TodoProps } from "../App";
 import { removeTask } from "../functions/removeTask";
+import { useState } from "react";
 
 const Task = ({ todos, setTodos }: TodoProps) => {
+  const [activeAccordion, setActiveAccordion] = useState<string | null>(null);
+
   const handleRemoveTask = (id: string) => {
     removeTask(id, { todos, setTodos });
+  };
+
+  const toggleAccordion = (id: string) => {
+    if (id === activeAccordion) {
+      setActiveAccordion(null);
+    } else {
+      setActiveAccordion(id);
+    }
+    console.log(activeAccordion);
   };
 
   return (
@@ -17,13 +29,24 @@ const Task = ({ todos, setTodos }: TodoProps) => {
         todos.map((todo) => {
           return (
             <div key={todo.id} className="task">
-              <section>
-                <h3>{todo.title}</h3>
-                <p>{todo.about}</p>
-              </section>
-              <button onClick={() => handleRemoveTask(todo.id)}>
-                <i className="fa-solid fa-xmark"></i>
-              </button>
+              <div className="display" onClick={() => toggleAccordion(todo.id)}>
+                <section>
+                  <h3>{todo.title}</h3>
+                  <p>{todo.about}</p>
+                </section>
+                <button
+                  onClick={() => handleRemoveTask(todo.id)}
+                  className="remove-btn"
+                >
+                  <i className="fa-solid fa-xmark"></i>
+                </button>
+              </div>
+              {activeAccordion === todo.id && (
+                <div className="addition-btns">
+                  <p>123</p>
+                  <p>456</p>
+                </div>
+              )}
             </div>
           );
         })
