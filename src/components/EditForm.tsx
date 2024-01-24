413c67bacfd08c8a39aff77f1107392b62b1ff7d
mishkamebonia <mishkas.mebonia@gmail.com>
2024-01-24 17:56:10 +0400

    refatored edit form component

--- src/components/EditForm.tsx
+++ src/components/EditForm.tsx
@@ -1,13 +1,15 @@
 import { useState } from "react";
 import { TodoObject } from "../App";
+import "./EditForm.scss";
 
 interface Props {
   todo: TodoObject;
   onEditTodo: (id: string, title: string, about: string) => void;
+  setEditAlert: React.Dispatch<React.SetStateAction<string | null>>;
 }
 
 const EditForm = (props: Props) => {
-  const { todo, onEditTodo } = props;
+  const { todo, onEditTodo, setEditAlert } = props;
 
   const [changedTitle, setChangedTitle] = useState(todo.title);
   const [changedAbout, setChangedAbout] = useState(todo.about);
@@ -21,20 +23,36 @@ const EditForm = (props: Props) => {
   };
 
   const onClickEditSubmit = (id: string) => {
-    console.log(changedTitle, changedAbout);
-    onEditTodo(id, changedTitle, changedAbout);
+    if (changedTitle !== "" && changedAbout !== "") {
+      onEditTodo(id, changedTitle, changedAbout);
+      setEditAlert(null);
+    }
+  };
+
+  const onCancel = () => {
+    setEditAlert(null);
   };
 
   return (
     <div>
-      <input type="text" value={changedTitle} onChange={changeTitleValue} />
-      <textarea
-        cols="30"
-        rows="10"
-        value={changedAbout}
-        onChange={changeAboutValue}
-      ></textarea>
-      <button onClick={() => onClickEditSubmit(todo.id)}>edit</button>
+      <div className="edit-form">
+        <input
+          type="text"
+          placeholder="Text..."
+          value={changedTitle}
+          onChange={changeTitleValue}
+        />
+        <textarea
+          placeholder="About..."
+          value={changedAbout}
+          onChange={changeAboutValue}
+        ></textarea>
+        <div className="row">
+          <button onClick={() => onCancel()}>Cancel</button>
+          <button onClick={() => onClickEditSubmit(todo.id)}>Save</button>
+        </div>
+      </div>
+      <div className="module-background" onClick={() => onCancel()}></div>
     </div>
   );
 };
